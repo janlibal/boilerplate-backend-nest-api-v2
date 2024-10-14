7a8acb4e13662494ba7bbcffeb328c4eb745d602
Jan Libal <libalj@seznam.cz>
2024-10-14 15:41:31 +0200

    Pre/Fri phase i. return

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -10,10 +10,8 @@ function configModuleSetup() {
   const configModule = ConfigModule.forRoot({
     isGlobal: true,
     load: [appConfig, redisConfig],//, authConfig, swaggerConfig, redisConfig],
-    //envFilePath: `${process.cwd()}/src/config/env/.env.${process.env.NODE_ENV}`
-    //envFilePath: `.env.${process.env.NODE_ENV}` 
-    //envFilePath: !ENV ? '.env' : `.env.${ENV}`,
-    envFilePath: ['.env']
+    envFilePath: `${process.cwd()}/src/config/env/.env.${process.env.NODE_ENV}`    
+    //envFilePath: ['.env']
   })
 
   return configModule
