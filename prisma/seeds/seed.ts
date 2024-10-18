69f8951c61fb22984ea78772939c1c96e2dcaad0
unknown <knoflik21@seznam.cz>
2024-10-18 14:48:31 +0200

    fix: Update seed

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -4,39 +4,48 @@ import crypto from '../../src/utils/crypto'
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.role.createMany({
-    data: [
-      {
-        role: 'admin',
-      },
-      {
-        role: 'user',
-      },
-    ],
-  })
+  const role = await prisma.role.findFirst()
+  if(!role) {
+    await prisma.role.createMany({
+      data: [
+        {
+          role: 'admin',
+        },
+        {
+          role: 'user',
+        },
+      ],
+    })
+  }
 
-  await prisma.status.createMany({
-    data: [
-      {
-        title: 'active',
-      },
-      {
-        title: 'inactive',
-      },
-    ],
-  })
+  const status = await prisma.status.findFirst()
+  if(!status) {
+    await prisma.status.createMany({
+      data: [
+        {
+          title: 'active',
+        },
+        {
+          title: 'inactive',
+        },
+      ],
+    })
+  }
 
-  await prisma.user.createMany({
-    data: [
-      {
-        id: 'fe918fd3-96b6-4573-ab9c-3c0c15a6a91d',
-        firstName: 'Joe',
-        lastName: 'Doe',
-        password: await crypto.hashPassword('Password123!'),
-        email: 'joe.doe@joedoe.com',
-      },
-    ],
-  })
+  const user = await prisma.user.findFirst()
+  if(!user) {
+    await prisma.user.createMany({
+      data: [
+        {
+          id: 'fe918fd3-96b6-4573-ab9c-3c0c15a6a91d',
+          firstName: 'Joe',
+          lastName: 'Doe',
+          password: await crypto.hashPassword('Password123!'),
+          email: 'joe.doe@joedoe.com',
+        },
+      ],
+    })
+  }
 }
 
 main()
