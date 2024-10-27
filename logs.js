d26b4294a7b60792d0ab1483d5c5dd0d5a35b093
Jan Libal <libalj@seznam.cz>
2024-10-27 15:54:00 +0100

    CI init

---
+++ logs.js
@@ -0,0 +1,14 @@
+console.log('NODE_ENV:', process.env.NODE_ENV)
+console.log('PORT:', process.env.APP_PORT)
+
+
+console.log('DB_USERNAME:',process.env.DB_USERNAME) 
+console.log('DB_PASSWORD: ',process.env.DB_PASSWORD)
+console.log('DB_NAME: ',process.env.DB_NAME)
+
+
+
+
+
+
+
