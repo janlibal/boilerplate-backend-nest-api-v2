f2fe7bd6c9ca02a3bbd0afcba58de2ee6803958c
Jan Libal <libalj@seznam.cz>
2024-10-28 11:58:53 +0100

    Update CI script

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -2,8 +2,8 @@
 set -e
 
 /opt/wait-for-it.sh postgres:5432
-yarn run migrate:deploy
-yarn run seed:prod
+yarn run migrate:test
+yarn run seed:test
 yarn run start:prod > prod.log 2>&1 &
 /opt/wait-for-it.sh localhost:4300
 yarn run test:e2e:new -- --runInBand
