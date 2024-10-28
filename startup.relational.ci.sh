077511753bcfe3532ff566ec5ccd7691bc4e1735
Jan Libal <libalj@seznam.cz>
2024-10-28 12:04:35 +0100

    Update package.json

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -6,4 +6,4 @@ yarn run migrate:test
 yarn run seed:test
 yarn run start:prod > prod.log 2>&1 &
 /opt/wait-for-it.sh localhost:4300
-yarn run test:e2e:new -- --runInBand
+yarn run test:e2e -- --runInBand
