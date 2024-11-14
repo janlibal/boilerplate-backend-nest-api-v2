eabb4f568154b9cc93955a3c1e3b0bfcf977933b
unknown <knoflik21@seznam.cz>
2024-11-14 10:00:44 +0100

    Update conflict errors

--- src/auth/constants/decorators.constants.ts
+++ src/auth/constants/decorators.constants.ts
@@ -19,4 +19,4 @@ export const unprocessableErrors = [{ "message": "hasToLoginViaProvider:apple"},
 
 export const badRequestSignUpErrors = [{"message": "Firstname has to be defined"},{"message": "Firstname must be longer than 1 char"},{"message": "Firstname must be a string"},{"message": "Firstname cannot be empty"},{"message": "Lastname has to be defined"},{"message": "Lastname must be longer than 1 char"},{"message": "Lastname must be a string"},{"message": "Lastname cannot be empty"},{"message": "Email must be in proper format"},{"message": "Email must be a string"},{"message": "Email cannot be empty"},{"message": "Password has to be defined"},{"message": "Password is too weak"},{"message": "Password can contain 20 characters at the most"},{"message": "Password must contain at least 6 characters"},{"message": "Password must be a string"}]
 
-export const conflictErrors = [{'message':'fake@email.com already exists'}, {$ref: getSchemaPath(LoginResponseDto)}]
+export const conflictErrors = [{'message':'fake@email.com already exists'}]
