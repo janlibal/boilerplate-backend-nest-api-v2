c24a94160ba5fde695ef1141734b4dc040efb7ab
Jan Libal <libalj@seznam.cz>
2024-11-19 09:22:21 +0100

    chore(consoleLogging): update targets

--- src/logger/providers/console.logging.ts
+++ src/logger/providers/console.logging.ts
@@ -4,12 +4,33 @@ export function consoleLoggingConfig(): Options {
     return {
       messageKey: 'msg',
       transport: {
-        target: 'pino-pretty',
-        options: {
-          singleLine: true,
-          ignore:
-            'req.id,req.method,req.url,req.headers,req.remoteAddress,req.remotePort,res.headers',
-        },
+        targets: [
+          {
+            target: 'pino-pretty',
+            level: 'debug',
+            options: {
+              singleLine: true,
+              colorize: true,
+              ignore: 'req.id,req.method,req.url,req.headers,req.remoteAddress,req.remotePort',
+            },
+          },
+          {
+            target: 'pino/file',
+            level: 'debug',
+            options: {
+              destination: './logs/app.log',
+              mkdir: true,
+            },
+          },
+          {
+            target: 'pino/file',
+            level: 'error',
+            options: {
+              destination: './logs/app-error.log',
+              mkdir: true,
+            },
+          },
+        ],
       },
     }
 }
