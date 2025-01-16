598faff8801eb2de37e0072b2f7a8910f05bc435
Jan Libal <libalj@seznam.cz>
2025-01-16 18:50:37 +0100

    chore(tests): init unit runs for auth service

---
+++ src/auth/__tests__/auth.service.test.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { vi, describe, beforeEach, it, expect } from 'vitest'
+import { AuthService } from '../auth.service'
+import { JwtService } from '@nestjs/jwt'
+import { UserService } from '../../users/user.service'
+import { UserModule } from '../../users/user.module'
+import { UserPersistenceModule } from '../../users/infrastructure/user.infrastructure.module'
+import { PrismaModule } from '../../database/prisma.module'
+import { SessionModule } from '../../session/session.module'
+import { SessionPersistenceModule } from '../../session/infrastructure/session.infrastructure.module'
+import { RedisModule } from '../../redis/redis.module'
+import configModuleSetup from 'src/config/config.module'
+import { RoleEnum } from '../../roles/roles.enum'
+import { StatusEnum } from '../../statuses/statuses.enum'
+import { dto } from './mock/auth.data'
+
+
+
+const mockUserService = {
+    create: vi.fn()
+}
+
+describe('UserService', () => {
+  let authService: AuthService
+  let userService: UserService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [UserModule, UserPersistenceModule, SessionModule, SessionPersistenceModule, RedisModule, PrismaModule, configModuleSetup()],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        }, 
+        JwtService
+      ],
+    }).compile()
+
+    authService = module.get<AuthService>(AuthService)
+    userService = module.get<UserService>(UserService)
+    
+
+    vi.restoreAllMocks()
+  })
+
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+
+  it('should be defined', () => {
+    expect(authService).toBeDefined()
+  })
+
+
+  it('register()', async () => {
+    mockUserService.create.mockResolvedValue(dto)
+    const result = await authService.register(dto)
+    expect(result).toBeUndefined()
+  })
+})
---
+++ src/auth/__tests__/mock/auth.data.ts
@@ -0,0 +1,3 @@
+import { AuthRegisterLoginDto } from "src/auth/dto/auth.register.login.dto";
+
+export const dto: AuthRegisterLoginDto = { firstName: 'Jan', lastName: 'Libal', password: 'Password123!', email: 'jan.libal@janlibal.com' }
