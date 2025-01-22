202f08b1237863beedb9457a5ab99d0f975a624e
unknown <knoflik21@seznam.cz>
2025-01-22 15:15:54 +0100

    chore(tests): add auth service unit runs

--- src/auth/__tests__/auth.service.test.ts
+++ src/auth/__tests__/auth.service.test.ts
@@ -9,19 +9,33 @@ import { PrismaModule } from '../../database/prisma.module'
 import { SessionModule } from '../../session/session.module'
 import { SessionPersistenceModule } from '../../session/infrastructure/session.infrastructure.module'
 import { RedisModule } from '../../redis/redis.module'
-import configModuleSetup from 'src/config/config.module'
-import { dto } from './mock/auth.data'
+import configModuleSetup from '../../config/config.module'
+import { dto, loginData, loginDataBad, mockSession, mockUser, mockUserGoogle, sessionData } from './mock/auth.data'
+import UnauthorizedError from '../../exceptions/unauthorized.exception'
+import UnprocessableError from '../../exceptions/unprocessable.exception'
+import crypto from '../../utils/crypto'
+import { SessionService } from '../../session/session.service'
 
-
+//vi.mock('../../utils/crypto', () => mockCrypto)
 
 const mockUserService = {
     create: vi.fn(),
     findByEmail: vi.fn()
 }
 
-describe('UserService', () => {
+const mockSessionService = {
+  create: vi.fn(),
+}
+
+const mockCrypto = {
+  comparePasswords: vi.fn(),
+  makeHash: vi.fn(),
+}
+
+describe('AuthService', () => {
   let authService: AuthService
   let userService: UserService
+  let sessionService: SessionService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -32,12 +46,17 @@ describe('UserService', () => {
           provide: UserService,
           useValue: mockUserService,
         }, 
+        {
+          provide: SessionService,
+          useValue: mockSessionService,
+        },
         JwtService
       ],
     }).compile()
 
     authService = module.get<AuthService>(AuthService)
     userService = module.get<UserService>(UserService)
+    sessionService = module.get<SessionService>(SessionService)
     
 
     vi.restoreAllMocks()
@@ -51,9 +70,57 @@ describe('UserService', () => {
     expect(authService).toBeDefined()
   })
 
-  it('register()', async () => {
-    mockUserService.create.mockResolvedValue(dto)
-    const result = await authService.register(dto)
-    expect(result).toBeUndefined()
+  describe('register()', () => {
+    it('should return new user', async () => {
+      mockUserService.create.mockResolvedValue(dto)
+      const result = await authService.register(dto)
+      expect(result).toBeUndefined()
+    })
+  })
+
+  describe('validateLogin()', () => {
+    it('should throw unauthorized if user is not found', async () => {
+      mockUserService.findByEmail.mockResolvedValue(null)
+
+      await expect(authService.validateLogin(loginData)).rejects.toThrow(
+        new UnauthorizedError('Invalid email or password'),
+      )
+    })
+
+    it('should throw Unauthorized if user provider is not email', async () => {
+      mockUserService.findByEmail.mockResolvedValue(mockUserGoogle)
+
+      await expect(authService.validateLogin(loginData)).rejects.toThrow(
+        new UnprocessableError(`hasToLoginViaProvider:${mockUserGoogle.provider}`),
+      )
+    })
+
+    it('should throw Unauthorized in case of missing password', async () => {
+      mockUserService.findByEmail.mockResolvedValue(mockUserGoogle)
+
+      await expect(authService.validateLogin(loginData)).rejects.toThrow(
+        new UnprocessableError('missingPassword')
+      )
+    })
+
+    it('should throw error for invalid password', async () => {
+      mockUserService.findByEmail.mockResolvedValue(mockUser)
+
+      const comparePasswordsSpy = vi
+        .spyOn(crypto, 'comparePasswords')
+        .mockResolvedValue(false)
+
+      await expect(authService.validateLogin(loginDataBad)).rejects.toThrow(
+        new UnauthorizedError('Invalid email or password')
+      )
+
+      expect(comparePasswordsSpy).toHaveBeenCalledWith(
+        loginDataBad.password,
+        mockUser.password,
+      )
+      expect(mockUserService.findByEmail).toHaveBeenCalledWith(
+        loginDataBad.email,
+      )
+    }) 
   })
 })
